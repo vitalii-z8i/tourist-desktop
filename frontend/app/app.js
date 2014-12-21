5415014efb4cd8c6de648e0bc1caba9aa60b3350
vtl <vitaliypol1992@gmail.com>
2014-12-22 01:36:16 +0200

    basic controllers and logic

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -2,6 +2,19 @@
 var $touristDesktop = angular.module('touristDesktop', [
   'ngRoute',
   'ngResource',
-  'ngMap',
   'directives',
 ]);
+
+$touristDesktop.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/places', {
+      controller:  'PlacesCtrl',
+    }).
+    when('/places/:id', {
+      controller:  'PlaceCtrl',
+      view:        'app/views/places/place.html'
+    }).
+    otherwise({
+      templateUrl: 'app/views/error.html'
+    });
+  }]);
